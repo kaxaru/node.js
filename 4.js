6a1b581af133fa5debb4dafb14c508b3389a0793
Kaxaru <khidechi@gmail.com>
2014-07-06 01:43:32 +0400

    Delete 4.js

--- 4.js
+++
@@ -1,7 +0,0 @@
-var fs = require('fs');
-var buf = fs.readFile(process.argv[2], function callback (err,data){
-    if(err) console.log(err);
-    var str = data.toString().split('\n').length;
-    console.log(str-1);
-});
-//console.log(buf);
