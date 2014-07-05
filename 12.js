5546725012e6dc1662822b1aaf83c82200f684dc
Kaxaru <khidechi@gmail.com>
2014-07-06 01:43:20 +0400

    Delete 12.js

--- 12.js
+++
@@ -1,8 +0,0 @@
-var http = require('http');
-var map = require('through2-map');
-
-http.createServer(function(req,res){
- req.pipe(map(function(chunk){
-   return chunk.toString().toUpperCase();
-})).pipe(res);
-}).listen(parseInt(process.argv[2]));
