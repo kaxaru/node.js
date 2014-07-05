46e67a1f52fbe45467d06d22e1f79b3bc184d564
Kaxaru <khidechi@gmail.com>
2014-07-06 01:43:44 +0400

    Delete 7.js

--- 7.js
+++
@@ -1,8 +0,0 @@
-var http = require('http');
-http.get(process.argv[2], function(response){
-    response.setEncoding('utf8');
-    response.on('err',console.error);
-    response.on('data',function(data){
-        console.log(data);
-    });
-})
